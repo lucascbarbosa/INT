ffc5a835916658dec9d0f6b9643eae05bb8c462c
Lucas <lucas.barbosa.0899@poli.ufrj.br>
2021-05-04 11:20:10 -0300

    update

---
+++ Manufatura Aditiva/Simulacao-GAN/Pipeline/3-Simulate_geometries/Comsol-matlab/main.m
@@ -0,0 +1,62 @@
+function [Es,model,approved] = main(iso_cutoff)
+
+    arrays_dir = 'C:/Users/lucas/Documents/Github/INT/Manufatura Aditiva/Simulação-GAN/Dados/Geometries/Arrays/';
+    young_dir = 'C:/Users/lucas/Documents/Github/INT/Manufatura Aditiva/Simulação-GAN/Dados/Mechanical_properties/E/';
+
+    datadirs = dir(arrays_dir); 
+    dircell = struct2cell(datadirs)' ;   
+    filenames = dircell(:,1);
+
+    Es = [];
+    approved = [];
+    
+    dtheta = 5;
+    theta_max = 45;
+    
+    for fid = (1:1)
+        datafilename =strcat(arrays_dir,filenames{fid+2}); 
+        f  = fopen(datafilename,'r');
+        data = textscan(f,'%s');
+        data = data{1};
+        size = sqrt(length(data));
+        fclose(f);
+        array = zeros(size);
+        row = [];
+
+        for i = (1:size)
+            for j = (1:size)
+                array(i,j) = str2double(data{(i-1)*size+j});
+            end
+        end
+        
+        for theta = 0:dtheta:theta_max
+            disp(theta);
+            E = 0;
+            try
+                [model,E] = simulate(array,theta);
+            catch
+                try
+                    disp(theta+0.1);
+                    [model,E] = simulate(array,theta+1);
+                catch
+                    try
+                        disp(theta-0.1);
+                    	[model,E] = simulate(array,theta-1);
+                    catch
+                        approved(fid) = false;
+                    end
+                end
+            end
+            Es(int8(theta/dtheta)+1) = E;
+        end
+        disp(Es);
+        m = min(Es);
+        M = max(Es);
+        iso = (M-m)/(M+m);
+        if iso <= iso_cutoff
+            approved(fid) = true;
+        else
+            approved(fid) = false;
+        end
+    end
+end
