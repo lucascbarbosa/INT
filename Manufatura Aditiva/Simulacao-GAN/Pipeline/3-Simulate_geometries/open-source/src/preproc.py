b02b2da1da02fad682f97a9c85ab6c2139118ba1
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2021-11-17 15:18:29 -0300

    update

--- Manufatura Aditiva/Simulacao-GAN/Pipeline/3-Simulate_geometries/open-source/src/preproc.py
+++
@@ -1,8 +0,0 @@
-import os
-from src.stl2vtk import stl2vtk
-
-def preproc(stl_filename,vtk_filename):
-    print('Preproc ',stl_filename)
-    stl2vtk(stl_filename,vtk_filename)
-    command = """python src/convert_mesh.py -d 3 "%s" "%s" """ %(vtk_filename,vtk_filename)
-    os.system(command)
