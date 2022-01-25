130692645ac1daf0ec01d31c7b7caa76eaa2d727
Lucas <40960766+lucascbarbosa@users.noreply.github.com>
2022-01-25 18:55:13 -0300

    updates compare.py

--- Manufatura Aditiva/Simulacao-GAN/Pipeline/3- Machine_learning/Analyse/compare.py
+++ Manufatura Aditiva/Simulacao-GAN/Pipeline/3- Machine_learning/Analyse/compare.py
@@ -1,32 +1,53 @@
 from mmap import ACCESS_WRITE
 import os 
 import numpy as np
+import sys
 
-open_source_dir = "D:/Lucas GAN/Dados/3- Mechanical_properties/young/p4/"
-comsol_dir = "D:/Lucas GAN/Dados/3- Mechanical_properties/young_COMSOL/"
+problem = str(sys.argv[1])
+dimension = int(sys.argv[2])
+origin = str(sys.argv[3])
+simmetry = str(sys.argv[4])
+
+if origin == "-r":
+    origin = "RTGA"
+
+if origin == "-g":
+    origin = "GAN"
+
+if os.getcwd().split('\\')[2] == 'lucas':
+    open_source_dir = f"E:/Lucas GAN/Dados/3- Mechanical_properties/{problem}/{origin}/{dimension}D/{simmetry}/"
+    comsol_dir = f"E:/Lucas GAN/Dados/3- Mechanical_properties/{problem}/MATLAB/{dimension}D/{simmetry}/"
+
+else:
+    open_source_dir = f"D:/Lucas GAN/Dados/3- Mechanical_properties/{problem}/{origin}/{dimension}D/{simmetry}/"
+    comsol_dir = f"D:/Lucas GAN/Dados/3- Mechanical_properties/{problem}/MATLAB/{dimension}D/{simmetry}/"
 
 os_filenames = os.listdir(open_source_dir)
 comsol_filenames = os.listdir(comsol_dir)
 
-youngs_os = np.array([])
+props_os = np.array([])
 for os_filename in os_filenames:
-    young = np.loadtxt(open_source_dir+os_filename)
-    youngs_os = np.append(youngs_os,np.array([(young[0]/young[1])]),axis=0)
-
-youngs_comsol = np.array([])
-for comsol_filename in comsol_filenames:
-    young = np.loadtxt(comsol_dir+comsol_filename)
-    youngs_comsol = np.append(youngs_comsol,np.array([young[0]/young[1]]),axis=0)
-
-youngs_comsol_ = []
-youngs_os_ = []
-for young_comsol,young_os in list(zip(youngs_comsol,youngs_os)):
-    if young_comsol == np.inf or young_comsol == 0 or young_comsol == np.nan or young_os == np.inf or young_os == 0 or young_os == np.nan:
+    try:
+        prop = np.loadtxt(open_source_dir+os_filename)
+        props_os = np.append(props_os,np.array([(prop[0]/prop[1])]),axis=0)
+    except:
         pass
-    else:
-        youngs_comsol_.append(young_comsol)
-        youngs_os_.append(young_os)
-youngs_os = np.array(youngs_os_)
-youngs_comsol = np.array(youngs_comsol_)
-# print(youngs_comsol[:7])
-print(np.round(np.sqrt(np.mean((youngs_comsol-youngs_os)**2)),2))
+
+print(props_os)
+# props_comsol = np.array([])
+# for comsol_filename in comsol_filenames:
+#     prop = np.loadtxt(comsol_dir+comsol_filename)
+#     props_comsol = np.append(props_comsol,np.array([prop[0]/prop[1]]),axis=0)
+
+# props_comsol_ = []
+# props_os_ = []
+# for young_comsol,young_os in list(zip(props_comsol,props_os)):
+#     if young_comsol == np.inf or young_comsol == 0 or young_comsol == np.nan or young_os == np.inf or young_os == 0 or young_os == np.nan:
+#         pass
+#     else:
+#         props_comsol_.append(young_comsol)
+#         props_os_.append(young_os)
+# props_os = np.array(props_os_)
+# props_comsol = np.array(props_comsol_)
+# # print(props_comsol[:7])
+# print(np.round(np.sqrt(np.mean((props_comsol-props_os)**2)),2))
